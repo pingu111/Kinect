a2c34bbbc28898584bbb9936495eff6d551e44ec
Paul <paul.turpin@telecom-sudgrenoble.eu>
2016-10-14 15:52:16 +0200

    state gestes

--- Assets/Gestes/Geste.cs
+++ Assets/Gestes/Geste.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public abstract class Geste
 {
+    private CurrentState triggerState;
+    private List<CurrentState> statePath;
+
     public abstract void OnStateChange();
 
     public abstract void GesteDetected();
